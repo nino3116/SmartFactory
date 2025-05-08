378a62432b5aab572ccdda8148aa9f9092b6eacb
nino3116 <gusn3116@gmail.com>
2025-05-08 13:25:55 +0900

    Update dependencies, enhance login page, and improve navbar links

--- SpringProject/src/main/java/com/project2/smartfactory/login/LoginController.java
+++ SpringProject/src/main/java/com/project2/smartfactory/login/LoginController.java
@@ -3,12 +3,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
+
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String loginpage() {
-        return "login";
+    public String loginPage() {
+        return "Login";
     }
     
 }
