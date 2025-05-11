3f634e9999624048d5ca1b43f895c6cb9f67e119
nino3116 <gusn3116@gmail.com>
2025-05-12 03:41:02 +0900

    install flowbite setting,  settings.json update

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -3,10 +3,13 @@ module.exports = {
   content: [
     "./src/**/*.{html,js,jsx,ts,tsx,vue,hbs,mustache,thymeleaf}", // Frontend 개발 파일
     "../SpringProject/src/main/resources/templates/**/*.{html,thymeleaf}", // Spring Boot 템플릿 파일 경로 (확장자에 따라 수정)
-    "./src/**/*.css",
+    "./src/**/*.css", // src에 costom할 css파일있으면 가져다 쓰기위해
+    "./node_modules/flowbite/**/*.js" // flowbite js파일을 쓰기위해 설정
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 };
