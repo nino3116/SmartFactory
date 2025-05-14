b32237c7d5b3770c6ff3a62d4841c50e796b3134
nino3116 <gusn3116@gmail.com>
2025-05-14 18:03:30 +0900

    Refactor security configuration and remove unused files
    - Deleted AppConfig.java as it was no longer needed.
    - Added SecurityConfig.java to manage security settings and authentication.
    - Updated CSS styles in style.css for better table and image presentation.
    - Modified admin_password_edit.html to enhance UI and added English title.
    - Removed form_errors.html as it was redundant.
    - Updated sidebar.html links for better navigation.
    - Deleted index.html as it was obsolete.
    - Adjusted base.html to include flowbite.min.js correctly.
    - Enhanced dashboard.html with new charts and improved data handling.
    - Updated defects.html to remove unnecessary meta tags.
    - Created realtime.html as a placeholder for future development.

--- SpringProject/src/main/java/com/project2/smartfactory/config/SecurityConfig.java
+++ SpringProject/src/main/java/com/project2/smartfactory/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-    package com.project2.smartfactory;
+    package com.project2.smartfactory.config;
 
     import org.springframework.context.annotation.Bean;
     import org.springframework.context.annotation.Configuration;
@@ -6,7 +6,9 @@
     import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
     import org.springframework.security.config.annotation.web.builders.HttpSecurity;
     import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-    import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
     import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
     @Configuration
@@ -47,6 +49,13 @@
             return http.build();
         }
 
+        
+        
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+        
         @Bean
         AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
             return authConfig.getAuthenticationManager();
